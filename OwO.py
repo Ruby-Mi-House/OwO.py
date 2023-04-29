5e259d908acc12a4e7b850591c3014aaa9b01cca
Anay <test@git.com>
2023-04-29 18:05:34 +0300

    SAS

--- OwO.py
+++ OwO.py
@@ -1,2 +1,37 @@
-print("Гык")
-
+class Conventer():
+    def __init__(self, chislo, valyta, lox):
+        self_chislo = chislo
+        self_valyta = valyta
+        self_lox = lox
+    def Chisla():
+        if self_chislo = 1:
+            return I
+        if self_chislo = 2:
+            return II
+        if self_chislo = 3:
+            return III
+        if self_chislo = 4:
+            return IV
+        if self_chislo = 5:
+            return V
+        if self_chislo = 5:
+            return V
+        if self_chislo = 6:
+            return VI
+        if self_chislo = 7:
+            return VII
+        if self_chislo = 8:
+            return VIII
+        if self_chislo = 9:
+            return IX 
+        if self_chislo = 10:
+            return X
+        else:
+            return "число больше 10"
+    def Valyta():
+        print("Ваша валута рубли")
+        a = self_valyta * 0,25
+        print("Если перевести рубли в лохов: ", a)
+    def Loxi():
+        n = input("Введите ваше имя и мы покажем его на французкий лад")
+        print(n, "по французки ", self_lox)
